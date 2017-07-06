d22721278804cd03f80c13ab4dc99dfe0786e066
Alex Nieto <alexnt05@hotmail.com>
2017-07-06 18:08:35 +0100

    Generate Walker for AnBx
    code for the AnBx file extension.

--- app/ui/HelloRunner.java
+++ app/ui/HelloRunner.java
@@ -1,5 +1,7 @@
 package ui;
 
+import org.antlr.runtime.tree.CommonTree;
+
 import handlers.AnBHandler;
 import handlers.AnBxHandler;
 
@@ -7,14 +9,16 @@ public class HelloRunner
     {
     	public static void main( String[] args) throws Exception 
     	{
-    		String filepath = "WMF";
+    		String filepath = "EPMO";
     		AnBHandler anbHandlerObject = new AnBHandler();
 //			anbHandlerObject.writeFile(anbHandlerObject.openFile("./study_cases/set_rev.AnB"));
-    		anbHandlerObject.analizeFile(anbHandlerObject.openFile("./study_cases/"+filepath+".AnB"));
+//    		anbHandlerObject.analizeFile(anbHandlerObject.openFile("./study_cases/"+filepath+".AnB"));
 			
 			AnBxHandler anbxHandlerObject = new AnBxHandler();
-			//anbxHandlerObject.writeFile(anbxHandlerObject.openFile("./study_cases/Amended_NSCK.AnBx"));
-			anbxHandlerObject.analizeFile(anbxHandlerObject.openFile("./study_cases/"+filepath+".AnBx"));   		
+			filepath = anbxHandlerObject.writeFile(anbxHandlerObject.openFile("./study_cases/"+filepath+".AnBx"));
+			System.out.println(filepath);
+//			anbxHandlerObject.analizeFile(anbxHandlerObject.openFile("./study_cases/"+filepath+".AnBx"));   		
+			
 			
     	}
     }
